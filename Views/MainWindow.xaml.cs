ede790047b52f11499a92cea20d210dc97934337
DurlaVerder <vrrrr227@gmail.com>
2024-11-24 22:45:59 +0300

    Ok

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 using System.Windows;
+using System.Windows.Input;
+using MaterialDesignThemes.Wpf;
 using Expense_Tracker.ViewModels;
 
 namespace Expense_Tracker.Views
@@ -8,6 +10,42 @@ namespace Expense_Tracker.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                MaximizeButton_Click(sender, e);
+            }
+            else
+            {
+                DragMove();
+            }
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+                MaximizeIcon.Kind = PackIconKind.WindowMaximize;
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+                MaximizeIcon.Kind = PackIconKind.WindowRestore;
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
